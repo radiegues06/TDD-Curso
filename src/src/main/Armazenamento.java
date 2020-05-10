49e208529dcd578963b39bd10d9c6eff735989c7
rafaelamanciodiegues@gmail.com <ri65%ma58R>
2020-05-10 15:26:24 -0300

    Método de Filtro dos dados por usuário

--- src/src/main/Armazenamento.java
+++ src/src/main/Armazenamento.java
@@ -1,13 +1,13 @@
 package main;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -16,6 +16,8 @@ public class Armazenamento {
 
     Document document;
 
+    XPath xpath = XPathFactory.newInstance().newXPath();
+
     public void load(String filePath) throws ParserConfigurationException, IOException, SAXException {
 
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -23,8 +25,33 @@ public class Armazenamento {
 
         //Normalize the XML Structure; It's just too important !!
         document.getDocumentElement().normalize();
+
     }
 
+    public HashMap<String,String> filterByUser(String nomeUsuario) {
+        HashMap<String,String> pontosDoUsuario = new HashMap<String,String>();
+        try {
+            XPathExpression searchXPath = xpath.compile("/Usuarios/Usuario[contains(Nome,'" + nomeUsuario + "')]/Pontos");
 
+            NodeList nodes = (NodeList) searchXPath.evaluate(document, XPathConstants.NODESET);
 
+            setPontosInNode(pontosDoUsuario, nodes);
+
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+        return pontosDoUsuario;
+    }
+
+    private void setPontosInNode(HashMap<String, String> pontos, NodeList nodes) {
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Element element = (Element) nodes.item(i);
+            pontos.put(getTagContent(element, "Tipo"),
+                    getTagContent(element, "Valor"));
+        }
+    }
+
+    private String getTagContent(Element element, String tagName) {
+        return element.getElementsByTagName(tagName).item(0).getTextContent();
+    }
 }
--- src/src/test/testArmazenamento.java
+++ src/src/test/testArmazenamento.java
@@ -4,10 +4,11 @@ import main.Armazenamento;
 import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import static org.junit.Assert.fail;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
 
 public class testArmazenamento {
 
@@ -38,4 +39,47 @@ public class testArmazenamento {
             fail();
         }
     }
+
+    @Test
+    public void whenFiltraPorNomeDoUsuarioInexistenteThenRetornaVazio() {
+        try {
+            armazenamento.load(CURR_DIR + "\\resources\\arquivoComUmUsuario.xml");
+            HashMap<String,String> pontos = armazenamento.filterByUser("invalido");
+            assertEquals(null, pontos.get("Moedas"));
+            assertEquals(null, pontos.get("Curtidas"));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void whenFiltraPorNomeDoUsuarioThenRetornaPontosDoUsuario() {
+        try {
+            armazenamento.load(CURR_DIR + "\\resources\\arquivoComUmUsuario.xml");
+            HashMap<String,String> pontos = armazenamento.filterByUser("Rafael");
+            assertEquals("15", pontos.get("Moedas"));
+            assertEquals("110", pontos.get("Curtidas"));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void whenFiltraPorNomeDoUsuarioComMaisDeUmUsuarioThenRetornaPontosDoUsuario() {
+        try {
+            armazenamento.load(CURR_DIR + "\\resources\\arquivoComDoisUsuarios.xml");
+            HashMap<String,String> pontos1 = armazenamento.filterByUser("Rafael");
+            assertEquals("15", pontos1.get("Moedas"));
+            assertEquals("110", pontos1.get("Curtidas"));
+
+            HashMap<String,String> pontos2 = armazenamento.filterByUser("Isabela");
+            assertEquals("3000", pontos2.get("Fichas"));
+            assertEquals("32", pontos2.get("Comentários"));
+            assertEquals("101", pontos2.get("Estrelas"));
+
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
 }
