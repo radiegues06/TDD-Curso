6827041799e9f9cf1f5cb81624f3deec5ca8648e
rafaelamanciodiegues@gmail.com <ri65%ma58R>
2020-05-16 10:22:26 -0300

    Testes de Placar para registrar pontos de usuário

--- src/src/main/Placar.java
+++ src/src/main/Placar.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Placar {
 
     ArmazenamentoInterface armazenamento;
@@ -15,4 +18,25 @@ public class Placar {
     public void registraPontos(String userName, String pointType, String pointValue) {
         armazenamento.setUserPoints(userName, pointType, pointValue);
     }
+
+    public HashMap<String,String> getUserPoints(String userName) {
+        return getValidHashMapValues(armazenamento.filterByUser(userName));
+    }
+
+    private HashMap<String,String> getValidHashMapValues(HashMap<String,String> inMap) {
+        HashMap<String,String> outMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : inMap.entrySet()) {
+
+           if (isValidEntry(entry)) {
+               outMap.put(entry.getKey(), entry.getValue());
+           }
+        }
+
+        return outMap;
+    }
+
+    private boolean isValidEntry(Map.Entry<String, String> entry) {
+        return !entry.getValue().equals("0");
+    }
+
 }
--- src/src/test/testePlacar.java
+++ src/src/test/testePlacar.java
@@ -1,6 +1,5 @@
 package test;
 
-import main.ArmazenamentoInterface;
 import main.MockArmazenamento;
 import main.Placar;
 import org.junit.Before;
@@ -24,7 +23,40 @@ public class testePlacar {
     public void whenRegistraUnicoUsuarioThenPassaInformacaoParaArmazenamento() {
         placar.registraPontos("Rafael", "Estrelas", "100");
         // Mock test
-        assertEquals("100", armazenamento.filterByUser("Rafael").get("Estrelas"));
+        assertEquals("100", armazenamento.filterByUserAndPointType("Rafael", "Estrelas"));
+    }
+
+    @Test
+    public void whenRegistraVariosUsuariosThenPassaInformacaoParaArmazenamento() {
+        placar.registraPontos("Rafael", "Fichas", "3000");
+        placar.registraPontos("Isabela", "Estrelas", "150");
+        placar.registraPontos("Júlia", "Curtidas", "18");
+
+        assertEquals("3000", armazenamento.filterByUserAndPointType("Rafael", "Fichas"));
+        assertEquals("150", armazenamento.filterByUserAndPointType("Isabela", "Estrelas"));
+        assertEquals("18", armazenamento.filterByUserAndPointType("Júlia", "Curtidas"));
+    }
+
+    @Test
+    public void whenRegistraValorEAtualizaThenPassaInformacaoParaArmazenamento() {
+        placar.registraPontos("Rafael", "Fichas", "3000");
+        placar.registraPontos("Rafael", "Estrelas", "15");
+        placar.registraPontos("Rafael", "Fichas", "1000");
+
+        assertEquals("1000", armazenamento.filterByUserAndPointType("Rafael", "Fichas"));
+    }
+
+    @Test
+    public void whenGetUserPointsThenRetornaTodosOsPontosValidosDoUsuario() {
+        placar.registraPontos("Rafael", "Estrelas", "15");
+        placar.registraPontos("Rafael", "Curtidas", "0");
+        placar.registraPontos("Rafael", "Vidas", "7");
+        placar.registraPontos("Rafael", "Fichas", "1500");
+        placar.registraPontos("Júlia", "Comentários", "30");
+        placar.registraPontos("Júlia", "Curtidas", "13");
+
+        assertEquals("15", placar.getUserPoints("Rafael").get("Estrelas"));
+        assertEquals(null, placar.getUserPoints("Rafael").get("Curtidas"));
     }
 
 }
